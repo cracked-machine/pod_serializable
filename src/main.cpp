ee63efc2f449ca94d271e09a5cea4fb13027cbe1
cracked-machine <chrisjsutton@gmail.com>
2025-07-18 15:16:06 +0000

    typos

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,21 @@ int main()
 {
     // serialize the POD-type object to binary file
     {
-        std::ofstream is{"pod.bin", is.binary};
-        if( not is.is_open() ) { return 1; }
+        std::ofstream os{"pod.bin", os.binary};
+        if( not os.is_open() ) { return 1; }
         
         PodRecord podRecord{ true, {}, "helloworld" };
-        is << podRecord;
-        is.flush();
+        os << podRecord;
+        os.flush();
     }
 
     // deserialize the POD-type object from binary file
     {
-        std::ifstream os{"pod.bin", os.binary};
-        if( not os.is_open() ) { return 1; }
+        std::ifstream is{"pod.bin", is.binary};
+        if( not is.is_open() ) { return 1; }
         
         PodRecord podRecord2{0, {}, "overwriteme"};
-        os >> podRecord2;
+        is >> podRecord2;
         std::cout << podRecord2.get_flag_one() << "\n";
         std::cout << podRecord2.get_field_one() << "\n";
     }

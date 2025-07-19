39d1459e51f24c775218a22a16d4d63d896e4400
cracked-machine <chrisjsutton@gmail.com>
2025-07-19 19:36:35 +0000

    perform clang-format and setup fsanitize

--- src/PodRecord.hpp
+++ src/PodRecord.hpp
@@ -1,61 +1,75 @@
-#include <ostream>
 #include <array>
 #include <iostream>
-#include <string>
+#include <ostream>
 #include <sstream>
+#include <string>
 
-class PodRecord
-{
+class PodRecord {
 private:
-
-//  | name  ||  flag  | reserved | field 1 | reserved | field 2 |
-//  -----------------------------------------------------------
-//  | bytes ||    1   |    10    |   10    |    10    |   10    |
-//
-//
+  //  | name  ||  flag  | reserved | field 1 | reserved | field 2 |
+  //  -----------------------------------------------------------
+  //  | bytes ||    1   |    10    |   10    |    10    |   10    |
+  //
+  //
 public:
-    std::array<char, 1> m_flag{};
-    std::array<char, 10> m_reserved_one{};
-    std::array<char, 10> m_field_one{};
-    std::array<char, 10> m_reserved_two{};
-    std::array<char, 10> m_field_two{};
+  std::array<char, 1> m_flag{};
+  std::array<char, 10> m_reserved_one{};
+  std::array<char, 10> m_field_one{};
+  std::array<char, 10> m_reserved_two{};
+  std::array<char, 10> m_field_two{};
 
-    friend std::ostream& operator<<(std::ostream& out, const PodRecord& obj) 
-    {
-        // writes must mirror member declaration order
-        out.write(obj.m_flag.data(), obj.m_flag.size());
-        out.write(obj.m_reserved_one.data(), obj.m_reserved_one.size());
-        out.write(obj.m_field_one.data(), obj.m_field_one.size());
-        out.write(obj.m_reserved_one.data(), obj.m_reserved_two.size());
-        out.write(obj.m_field_two.data(), obj.m_field_two.size());
-        return out;
-    }
+  /**
+   * @brief Serialize the object reference into the given stream
+   * 
+   * @param out The stream to serialize to 
+   * @param obj The object to serialize from 
+   * @return std::ostream& 
+   */
+  friend std::ostream &operator<<(std::ostream &out, const PodRecord &obj) {
+    // writes must mirror member declaration order
+    out.write(obj.m_flag.data(), obj.m_flag.size());
+    out.write(obj.m_reserved_one.data(), obj.m_reserved_one.size());
+    out.write(obj.m_field_one.data(), obj.m_field_one.size());
+    out.write(obj.m_reserved_one.data(), obj.m_reserved_two.size());
+    out.write(obj.m_field_two.data(), obj.m_field_two.size());
+    return out;
+  }
 
-    friend std::istream& operator>>(std::istream& in, PodRecord& obj)
-    {
-        // reads must mirror member declaration order
-        in.read(obj.m_flag.data(), obj.m_flag.size());
-        in.read(obj.m_reserved_one.data(), obj.m_reserved_one.size());
-        in.read(obj.m_field_one.data(), obj.m_field_one.size());
-        in.read(obj.m_reserved_two.data(), obj.m_reserved_two.size());
-        in.read(obj.m_field_two.data(), obj.m_field_two.size());
-        return in;
-    }
+  /**
+   * @brief Deserialise the given stream into the object reference
+   * 
+   * @param in The stream to deserialize from
+   * @param obj The object to receive the deserialization
+   * @return std::istream& 
+   */
+  friend std::istream &operator>>(std::istream &in, PodRecord &obj) {
+    // reads must mirror member declaration order
+    in.read(obj.m_flag.data(), obj.m_flag.size());
+    in.read(obj.m_reserved_one.data(), obj.m_reserved_one.size());
+    in.read(obj.m_field_one.data(), obj.m_field_one.size());
+    in.read(obj.m_reserved_two.data(), obj.m_reserved_two.size());
+    in.read(obj.m_field_two.data(), obj.m_field_two.size());
+    return in;
+  }
 
-    // Universal member-to-string accessor.
-    // pod_member: Pass in POD **object** member to get the string repr.
-    // to_ascii: The output format. ascii if true, byte/hex if false.
-    template<size_t SIZE>
-    std::string get(
-        const std::array<char, SIZE>& pod_member, 
-        bool to_ascii
-    )
-    {
-        std::stringstream out;
-        for( const char &b: pod_member) { 
-            if (to_ascii) { out << std::dec << b; }
-            else { out << std::hex << unsigned(b); }
-        }
-        return out.str();
+  /**
+   * @brief Returns member value as formatted string.
+   * 
+   * @tparam SIZE 
+   * @param obj_member The member to get as string
+   * @param to_ascii if true format output as ascii text, else hex/bytes
+   * @return std::string 
+   */
+  template <size_t SIZE>
+  std::string get(const std::array<char, SIZE> &obj_member, bool to_ascii) {
+    std::stringstream out;
+    for (const char &b : obj_member) {
+      if (to_ascii) {
+        out << std::dec << b;
+      } else {
+        out << std::hex << unsigned(b);
+      }
     }
+    return out.str();
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,38 @@
+#include <PodRecord.hpp>
 #include <fstream>
 #include <ostream>
-#include <PodRecord.hpp>
 
-int main()
-{
-    // serialize the POD-type object to binary file
-    {
-        std::ofstream os{"pod.bin", os.binary};
-        if( not os.is_open() ) { return 1; }
-        
-        PodRecord podRecord { 
-            true, 
-            {}, // reserved
-            {0xD,0xE,0xA,0xD, 0xB, 0xE, 0xE, 0xF},
-            {}, // reserved
-            "1.1.1.1"
-        };
-        os << podRecord;
-        os.flush();
+int main() {
+  // serialize the POD-type object to binary file
+  {
+    std::ofstream os{"pod.bin", os.binary};
+    if (not os.is_open()) {
+      return 1;
     }
 
-    // deserialize the POD-type object from binary file
-    {
-        std::ifstream is{"pod.bin", is.binary};
-        if( not is.is_open() ) { return 1; }
-        
-        PodRecord podRecord2;
-        is >> podRecord2;
-    
-        std::cout << podRecord2.get(podRecord2.m_flag, false) << "\n";
-        std::cout << podRecord2.get(podRecord2.m_field_one, false) << "\n";
-        std::cout << podRecord2.get(podRecord2.m_field_two, false) << "\n";
+    PodRecord podRecord{true,
+                        {}, // reserved
+                        {0xD, 0xE, 0xA, 0xD, 0xB, 0xE, 0xE, 0xF},
+                        {}, // reserved
+                        "1.1.1.1"};
+    os << podRecord;
+    os.flush();
+  }
+
+  // deserialize the POD-type object from binary file
+  {
+    std::ifstream is{"pod.bin", is.binary};
+    if (not is.is_open()) {
+      return 1;
     }
-    
-    return 0;
+
+    PodRecord podRecord2;
+    is >> podRecord2;
+
+    std::cout << podRecord2.get(podRecord2.m_flag, false) << "\n";
+    std::cout << podRecord2.get(podRecord2.m_field_one, false) << "\n";
+    std::cout << podRecord2.get(podRecord2.m_field_two, false) << "\n";
+  }
+
+  return 0;
 }
